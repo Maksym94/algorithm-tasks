7087b8178b0a069af0750ecceb4d910bca9ab5aa
Maksym Petrenko <sawjkee@ukr.net>
2020-11-09 18:51:25 +0200

    Task 67. Add Binary. Refactoring

--- src/main/java/strings/add/binary_67/Solution.java
+++ src/main/java/strings/add/binary_67/Solution.java
@@ -3,43 +3,29 @@ package strings.add.binary_67;
 public class Solution {
 
     public String addBinary(String a, String b) {
-        char[] aChars = a.toCharArray();
-        char[] bChars = b.toCharArray();
-        int aIdx = aChars.length - 1;
-        int bIdx = bChars.length - 1;
-        char carry = '0';
+        int aIdx = a.length() - 1;
+        int bIdx = b.length() - 1;
+        int carry = 0;
         StringBuilder sb = new StringBuilder();
         while (aIdx >= 0 || bIdx >= 0) {
-            char aVal = aIdx >= 0 ? aChars[aIdx] : '0';
-            char bVal = bIdx >= 0 ? bChars[bIdx] : '0';
+            int aVal = aIdx >= 0 ? Character.getNumericValue(a.charAt(aIdx)) : 0;
+            int bVal = bIdx >= 0 ? Character.getNumericValue(b.charAt(bIdx)) : 0;
 
-
-            if (aVal == '1' && bVal == '1') {
-                if (carry == '1') {
-                    sb.append('1');
-                } else {
-                    sb.append('0');
-                    carry = '1';
-                }
-            } else if (aVal != bVal) {
-                if (carry == '1') {
-                    sb.append('0');
-                } else {
-                    sb.append('1');
-                }
+            int sum = aVal + bVal + carry;
+            if (sum > 2) {
+                sb.append(1);
+            } else if (sum > 1) {
+                sb.append(0);
+                carry = 1;
             } else {
-                if (carry == '1') {
-                    sb.append('1');
-                    carry = '0';
-                } else {
-                    sb.append('0');
-                }
+                sb.append(sum);
+                carry = 0;
             }
             aIdx--;
             bIdx--;
         }
-        if (carry == '1') {
-            sb.append('1');
+        if (carry == 1) {
+            sb.append(1);
         }
         return sb.reverse().toString();
     }
